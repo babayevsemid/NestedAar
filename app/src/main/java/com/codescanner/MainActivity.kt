e12d6a28985c6fe6f23acea24088d11617ab4587
Samid Babayev <samid@Samids-MacBook-Pro.local>
2022-02-24 21:56:59 +0400

    Fix issue

--- app/src/main/java/com/codescanner/MainActivity.kt
+++ app/src/main/java/com/codescanner/MainActivity.kt
@@ -2,11 +2,11 @@ package com.codescanner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.semid.qrcodescanner.CodeScannerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 }
