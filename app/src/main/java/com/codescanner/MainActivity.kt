c38449d6f5e89f42f2b014e05d6aaae3f8c5103e
Samid Babayev <developer_i5@Samid-iMac.local>
2022-03-01 15:35:39 +0400

    Fix issues

--- app/src/main/java/com/codescanner/MainActivity.kt
+++ app/src/main/java/com/codescanner/MainActivity.kt
@@ -2,7 +2,6 @@ package com.codescanner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.semid.qrcodescanner.CodeScannerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
